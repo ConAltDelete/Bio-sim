4d3d7a211213c37f9d07a946ebb0fd249ee1069c
roy__ <roygra@nmbu.no>
2021-01-05 11:42:12 +0100

    added cell

--- biosim/island.py
+++ biosim/island.py
@@ -6,5 +6,10 @@
 __author__ = 'Roy Erling Granheim, Mats Hoem Olsen'
 __email__ = 'roy.erling.granheim@nmbu.no, matshoemolsen@nmbu.no'
 
+
+def cell():
+    pass
+
+
 if __name__ == '__main__':
     pass
