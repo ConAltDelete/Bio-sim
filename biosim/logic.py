3b440e1d247a762ddfcf78455c260ad60454b609
Mats Hoem Olsen <mats.hoem.olsen@nmbu.no>
2021-01-08 11:39:54 +0100

    testing migration

--- biosim/logic.py
+++ biosim/logic.py
@@ -237,7 +237,7 @@ if __name__ == '__main__':
     this_fucking_thing = simulation.BioSim(island_map = "WWWW\nWLLW\nWWWW", ini_pop = [{'loc':(2,2) , 'pop':[ {"species":"herbivore","age":5,"weight":20} for _ in range(10)] } ] , seed = 1234)
     the_map = this_fucking_thing.island
     illigal_moves = this_fucking_thing.illigal_coord
-    for _ in range(3):
+    for _ in range(30):
         season_migration(the_map,illigal_moves)
     for cell in the_map:
         print("coord:",the_map[cell].coord,"animals:",the_map[cell].herb_default)
