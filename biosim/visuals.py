ce9dade864b66ef6ca09c0c38cbd090fe59275d0
Mats Hoem Olsen <mats.hoem.olsen@nmbu.no>
2021-01-09 19:16:09 +0100

    Added some error detection in visual.py

--- biosim/visuals.py
+++ biosim/visuals.py
@@ -18,13 +18,23 @@ def string2map(map_str: str):
     :return: a new map, and illigal coordinates
     """
     map_list = [list(map_r) for map_r in map_str.split()]
+
+    x_length = len(map_list[0])
+    for row in map_list:
+        if len(row) != x_length:
+            raise ValueError("Inconsistent row lenght")
+    
+    standard_values = {"W": 0, "L": 3, "H": 2, "D": 1}
+
     new_map = {}
     illigal_coord = []
     for row in enumerate(map_list):
         for colum in enumerate(row[1]):
             if colum[1] != "W":
+                if colum[1] not in standard_values:
+                    raise ValueError("'{}' is not a standard value, expected {}".format(colum[1],standard_values))
                 new_map.update({(colum[0] + 1, row[0] + 1): Cells(
-                    {"W": 0, "L": 3, "H": 2, "D": 1}[colum[1]], [colum[0] + 1, row[0] + 1])})
+                    standard_values[colum[1]], [colum[0] + 1, row[0] + 1])})
             else:
                 illigal_coord.append((colum[0] + 1, row[0] + 1))
     return [new_map, illigal_coord]
