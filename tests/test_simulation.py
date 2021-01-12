5f4c6195beb74e68220879745cf1b9879a202aee
Mats Hoem Olsen <mats.hoem.olsen@nmbu.no>
2021-01-12 14:35:17 +0100

    Added tests for two species

--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -8,8 +8,25 @@ def test_consistensy_simulation_one_cell_one_species():
 			{
 		"species": "Herbivore",
 		"age": 5,
-		"weight": 20} for _ in range(2)]
-	}], seed=1234)
+		"weight": 20} for _ in range(100)]
+	}])
 	sim.simulate(num_years=200,vis_years=1)
 	n_animals = len(sim.island[(2,2)].default["Herbivore"])
 	assert n_animals == pt.approx(200,abs=25)
+
+def test_consistensy_simulation_one_cell_two_species():
+	ini_herb = [
+			{
+		"species": "Herbivore",
+		"age": 5,
+		"weight": 20} for _ in range(100)]
+	ini_carn = [{"species": "Carnivore",
+		"age": 5,
+		"weight": 20} for _ in range(20)]
+	ini_pop = [{"loc":(2,2),"pop":ini_carn + ini_herb}]
+	sim = BioSim(island_map="WWW\nWLW\nWWW",ini_pop=ini_pop)
+	sim.simulate(num_years=300,vis_years=1)
+	n_herb = len(sim.island[(2,2)].default["Herbivore"])
+	n_carn = len(sim.island[(2,2)].default["Carnivore"])
+	assert n_herb == pt.approx(85,abs=16)
+	assert n_carn == pt.approx(41, abs=9)
