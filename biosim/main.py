6c235c62cbb14deb4e6a92816b5745b01eeb2bb7
Roy <roygra@nmbu.no>
2021-01-13 23:24:22 +0100

    First version of integrating visualization.py into simulation.py still missing data collection

--- biosim/main.py
+++ biosim/main.py
@@ -6,4 +6,29 @@
 from biosim.simulation import *
 
 if __name__ == "__main__":
-    pass
+    sim = BioSim(island_map=""" WWWWW
+                                WLLLW
+                                WLLLW
+                                WLLLW
+                                WWWWW""",ini_pop=[{
+        "loc":(2,2),
+        "pop":[
+            {
+        "species": "Herbivore",
+        "age": 5,
+        "weight": 20} for _ in range(10)]
+    }])
+    sim.simulate(num_years=50,vis_years=1)
+    sim.add_population(population=[
+        {
+            "loc":(2,2),
+            "pop":[
+                {
+                    "species": "Carnivore",
+                    "age": 5,
+                    "weight": 20
+                } for _ in range(5)
+            ]
+        }
+    ])
+    sim.simulate(num_years=250,vis_years=1)
